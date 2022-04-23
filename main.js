3f3b99e5898cfd7e6467f9ed611d5c9de5a59baf
Khushi2525 <85677072+Khushi2525@users.noreply.github.com>
2022-04-23 20:49:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+lipstick_x = 0;
+lipstick_y = 0; 
+
+function preload(){
+    lipstick = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    
+    poseNet.on('pose',gotPoses);
+    }
+    
+    function modelLoaded(){
+        console,log('PoseNet is Initialized')
+    }
+    
+    function gotPoses(results){
+        if (results.length > 0) {
+            console.log(results);
+            console.log("nose x = " + results[0].pose.nose.x);
+            console.log("nose x = " + results[0].pose.nose.y);
+    
+            lipstick_x = results[0].pose.nose.x-30;
+            lipstick_y = results[0].pose.nose.y+10;
+        }
+    }
+
+    function draw(){
+        image(video, 0, 0, 300, 300);
+        image(lipstick, lipstick_x, lipstick_y,60, 45);
+
+    }
+
+function take_snapshot(){
+    save("my_photo.png");
+}
